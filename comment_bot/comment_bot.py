8d291a7092be7ba2a21cf20c128f0bfee551c3c0
himi64 <himi64@hotmail.com>
2019-04-17 22:06:00 -0400

    reddit comment bot initial commit

--- comment_bot/comment_bot.py
+++ comment_bot/comment_bot.py
@@ -1,39 +1,46 @@
 import praw
-import config
+# import config
 import time
 import os
 import json
 
-def bot_login():
-	print "Logging in..."
-	r = praw.Reddit(username = config.username,
-				password = config.password,
-				client_id = config.client_id,
-				client_secret = config.client_secret,
-				user_agent = "The Reddit Commenter v1.0")
-	print "Logged in!"
-
+def bot_login(credfile):
+	
+	credfile = 'credfile.json'
+	credfile_prefix = ''
+	
+	print("Logging in...")
+	
+	# Read credentials to a dictionary
+	with open(credfile) as fh:
+		creds = json.loads(fh.read())
+		
+	r = praw.Reddit(client_id=creds['client_id'], client_secret=creds['client_secret'], user_agent=creds['user_agent'])
+	
+	print("Logged in!")
+	
 	return r
 
+
 def run_bot(r, comments_replied_to):
-	print "Searching last 1,000 comments"
+	print("Searching last 1,000 comments")
 
 	for comment in r.subreddit('test').comments(limit=1000):
 		if "sample user comment" in comment.body and comment.id not in comments_replied_to and comment.author != r.user.me():
-			print "String with \"sample user comment\" found in comment " + comment.id
+			print("String with \"sample user comment\" found in comment " + comment.id)
 			comment.reply("Hey, I like your comment!")
-			print "Replied to comment " + comment.id
+			print("Replied to comment " + comment.id)
 
 			comments_replied_to.append(comment.id)
 
 			with open ("comments_replied_to.txt", "a") as f:
 				f.write(comment.id + "\n")
 
-	print "Search Completed."
+	print("Search Completed.")
 
-	print comments_replied_to
+	print(comments_replied_to)
 
-	print "Sleeping for 10 seconds..."
+	print("Sleeping for 10 seconds...")
 	#Sleep for 10 seconds...		
 	time.sleep(10)
 
@@ -48,9 +55,10 @@ def get_saved_comments():
 
 	return comments_replied_to
 
-r = bot_login()
+credfile = 'credfile.json'
+r = bot_login(credfile)
 comments_replied_to = get_saved_comments()
-print comments_replied_to
+print(comments_replied_to)
 
 while True:
 	run_bot(r, comments_replied_to)
